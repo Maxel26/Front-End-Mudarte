6648adddc22517e22489a0d0538dbd8e1a20a390
unknown <Maxel26>
2023-07-14 13:07:57 -0500

    [+] componentes : (inicio,nosotros,preguntas,productos), y carpeta Shared con : (header,footer)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ProductosComponent } from './productos/productos.component';
+import { PreguntasComponent } from './preguntas/preguntas.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    ProductosComponent,
+    PreguntasComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
